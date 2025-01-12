f4a1117e7f115df59f65cf7c70f3b7ef4a70396e
RexxzynXD <150516773+RexxHayanasi@users.noreply.github.com>
2025-01-13 04:36:12 +0700

    Update dynamic-title.ts

--- src/helpers/dynamic-title.ts
+++ src/helpers/dynamic-title.ts
@@ -3,8 +3,8 @@ import { useEffect } from "react";
 export const useDynamicTitle = (loading: boolean, episodeTitle?: string) => {
   useEffect(() => {
     document.title = loading
-      ? "Loading... | Otakudesu"
-      : `${episodeTitle} | Otakudesu`;
+      ? "Loading... | Archavelianime"
+      : `${episodeTitle} | Archavelianime`;
 
     return () => {
       document.title = "Otakudesu";
