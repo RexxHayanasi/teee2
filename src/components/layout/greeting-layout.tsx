8c4964bd2d0333eabfb96ea253fe05eecbfabc17
RexxzynXD <150516773+RexxHayanasi@users.noreply.github.com>
2025-01-13 05:10:27 +0700

    Update greeting-layout.tsx

--- src/components/layout/greeting-layout.tsx
+++ src/components/layout/greeting-layout.tsx
@@ -15,39 +15,39 @@ export default function GreetingLayout() {
       <div className="mx-auto max-w-7xl px-12">
         <div className="mx-auto w-full text-left md:w-11/12 md:text-center xl:w-9/12">
           <h1 className="mb-8 text-4xl font-extrabold leading-none tracking-normal text-foreground md:text-6xl md:tracking-tight">
-            <span>Begin an exciting journey&nbsp;</span>
+            <span>Selamat Datang di Website Anime Streaming Gratis&nbsp;</span>
             <span className="leading-12 block w-full bg-gradient-to-r from-green-400 to-blue-500 bg-clip-text py-2 text-transparent lg:inline">
-              into the world of anime!
+              Archave Community!
             </span>
           </h1>
           <p className="mb-8 px-0 text-lg font-medium text-foreground md:text-xl lg:px-24">
-            Start watching an anime without ads! From the{" "}
+            Mulailah menonton anime gratis tanpa iklan disini,Archavelianime dibuat dari{" "}
             <span className="leading-12 bg-gradient-to-r from-orange-400 to-red-500 bg-clip-text py-2 font-bold text-transparent">
               community
             </span>
-            , for the{" "}
+            , untuk{" "}
             <span className="leading-12 bg-gradient-to-r from-orange-400 to-red-500 bg-clip-text py-2 font-bold text-transparent">
               community
             </span>
-            , by the{" "}
+            , dan untuk {" "}
             <span className="leading-12 bg-gradient-to-r from-orange-400 to-red-500 bg-clip-text py-2 font-bold text-transparent">
-              community
+              semua orang ✨
             </span>
             {""}.
           </p>
           <div className="mb-4 sm:space-x-2 md:mb-8 md:flex md:w-full md:justify-center md:space-x-2">
             <Link
-              href="#started"
+              href=""
               className="mb-2 inline-flex w-full items-center justify-center gap-2 rounded-2xl bg-blue-400/50 px-6 py-3 text-lg font-medium text-foreground duration-300 hover:shadow-lg hover:shadow-blue-300/50 dark:bg-blue-400/20 hover:dark:shadow-blue-300/10 sm:mb-0 sm:w-auto"
             >
-              <span className={title()}>Get Started</span>
+              <span className={title()}>Memulai Menonton</span>
               <FaArrowRight />
             </Link>
             <Link
               href="/anime"
               className="mb-2 inline-flex w-full items-center justify-center gap-2 rounded-2xl bg-gray-100 px-6 py-3 text-lg font-medium text-foreground duration-300 hover:shadow-lg hover:shadow-gray-300/50 dark:bg-gray-600/30 dark:text-white hover:dark:shadow-white/10 sm:mb-0 sm:w-auto"
             >
-              <span className={title()}>Search Anime</span>
+              <span className={title()}>Cari Anime</span>
               <FaSearch />
             </Link>
           </div>
