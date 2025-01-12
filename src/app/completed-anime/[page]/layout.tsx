d1a2ae8821a3051c7b0b4388ac6f192fd0039488
RexxzynXD <150516773+RexxHayanasi@users.noreply.github.com>
2025-01-13 04:35:16 +0700

    Update layout.tsx

--- src/app/completed-anime/[page]/layout.tsx
+++ src/app/completed-anime/[page]/layout.tsx
@@ -2,8 +2,8 @@ import BaseLayout from "@/components/layout/base-layout";
 import { Metadata } from "next/types";
 
 export const metadata: Metadata = {
-  title: "Completed Anime | Otakudesu",
-  description: "Completed Anime Page Otakudesu. Build by Rizky Haksono",
+  title: "Completed Anime | Archavelianime",
+  description: "Completed Anime Page Otakudesu. Build by RexxzynXD",
 };
 
 export default function CompletedAnimeLayout({
