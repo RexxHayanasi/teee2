ff40cbc2ebab6e0eb07c764a5d69c2497a10ab53
RexxzynXD <150516773+RexxHayanasi@users.noreply.github.com>
2025-01-13 04:39:50 +0700

    Update layout.tsx

--- src/app/genres/layout.tsx
+++ src/app/genres/layout.tsx
@@ -2,8 +2,8 @@ import BaseLayout from "@/components/layout/base-layout";
 import { Metadata } from "next/types";
 
 export const metadata: Metadata = {
-  title: "Genres | Otakudesu",
-  description: "Genres Page Otakudesu. Build by Rizky Haksono",
+  title: "Genres | Archavelianime",
+  description: "Genres Page Otakudesu. Build by RexxzynXD",
 };
 
 export default function GenreLayout({
