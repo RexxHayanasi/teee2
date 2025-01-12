806c4a762a48b1d38baf7e56b37532922533fa86
RexxzynXD <150516773+RexxHayanasi@users.noreply.github.com>
2025-01-13 04:32:08 +0700

    Update layout.tsx

--- src/app/anime/layout.tsx
+++ src/app/anime/layout.tsx
@@ -2,8 +2,8 @@ import BaseLayout from "@/components/layout/base-layout";
 import { Metadata } from "next/types";
 
 export const metadata: Metadata = {
-  title: "Search Anime | Otakudesu",
-  description: "Anime Page Otakudesu. Build by Rizky Haksono",
+  title: "Search Anime | Archavelianime",
+  description: "Anime Page. Build by RexxzynXD",
 };
 
 export default function AnimeSearchLayout({
