3fbdd90349041c8d878f0f955c2a6b51af191638
RexxzynXD <150516773+RexxHayanasi@users.noreply.github.com>
2025-01-13 04:44:01 +0700

    Update layout.tsx

--- src/app/ongoing-anime/[page]/layout.tsx
+++ src/app/ongoing-anime/[page]/layout.tsx
@@ -2,8 +2,8 @@ import BaseLayout from "@/components/layout/base-layout";
 import { Metadata } from "next/types";
 
 export const metadata: Metadata = {
-  title: "On Going Anime | Otakudesu",
-  description: "Ongoing Anime Page Otakudesu. Build by Rizky Haksono",
+  title: "On Going Anime | Archavelianime",
+  description: "Ongoing Anime Page Otakudesu. Build by RexxzynXD",
 };
 
 export default function OngoingAnimeLayout({
