af4adae6e281526b86f3bc78d010f14ed1f1bf1a
RexxzynXD <150516773+RexxHayanasi@users.noreply.github.com>
2025-05-14 21:49:39 +0700

    Update skeleton-card.tsx

--- src/components/layout/skeleton-card.tsx
+++ src/components/layout/skeleton-card.tsx
@@ -2,14 +2,42 @@ import { Skeleton } from "../ui/skeleton";
 
 export default function SkeletonCard() {
   return (
-    <div className="container mx-auto my-10 flex flex-col space-y-3">
-      <Skeleton className="h-[125px] w-full rounded-xl" />
-      <div className="space-y-2">
-        <Skeleton className="h-4 w-full" />
-        <Skeleton className="h-4 w-4/5" />
-        <Skeleton className="h-4 w-2/3" />
-        <Skeleton className="h-4 w-1/2" />
+    <div className="flex flex-col space-y-3">
+      {/* Gambar */}
+      <Skeleton className="aspect-[3/4] w-full rounded-t-lg" />
+      
+      {/* Konten */}
+      <div className="p-4 space-y-3">
+        {/* Judul */}
+        <Skeleton className="h-5 w-full rounded-md" />
+        
+        {/* Status dan Type */}
+        <div className="flex gap-2">
+          <Skeleton className="h-4 w-16 rounded-full" />
+          <Skeleton className="h-4 w-12 rounded-full" />
+        </div>
+        
+        {/* Rating */}
+        <Skeleton className="h-4 w-10 rounded-full" />
+        
+        {/* Genre */}
+        <div className="flex flex-wrap gap-2 pt-2">
+          <Skeleton className="h-4 w-16 rounded-full" />
+          <Skeleton className="h-4 w-14 rounded-full" />
+          <Skeleton className="h-4 w-12 rounded-full" />
+        </div>
       </div>
+    </div>
+  );
+}
+
+// Versi alternatif untuk loading di tempat lain
+export function SkeletonList() {
+  return (
+    <div className="grid gap-4 max-[400px]:grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6">
+      {[...Array(12)].map((_, index) => (
+        <SkeletonCard key={index} />
+      ))}
     </div>
   );
 }
